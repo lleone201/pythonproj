4080ae35ec72d7022c85856927360b35670b9176
offendo <nilaypatel2@gmail.com>
2019-03-25 15:15:48 -0400

    modified tickers.py to read from valid_tickers.csv instead of recompute valid tickers each time

--- tickers.py
+++ tickers.py
@@ -1,23 +1,45 @@
 import requests
-from iex import reference
+import iex
 import pandas as pd
 from io import StringIO
 import sys
 
+# wrote just to save time finding valid tickers
 
-def is_valid(tickers):
-    s = set(reference.symbols()['symbol'])
-    return [t.upper() in s for t in tickers]
+
+def get_valid_tickers(outfile='valid_tickers.csv'):
+    """
+    Gets all valid tickers into a file 'valid_tickers.csv' to save computation time.
+    """
+    url = r'https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download'
+    data = requests.get(url, allow_redirects=True)
+    # read csv from a string
+    df = pd.read_csv(StringIO(data.text))
+    newdf = df[filter_valid(df['Symbol'])].loc[:, 'Symbol']
+    newdf.to_csv('valid_tickers.csv')
+
+
+def filter_valid(tickers):
+    """
+    Returns a list of bools, the ith of which indicates whether the ith ticker in the input is valid or not.
+    """
+    valid_tickers = set(pd.read_csv(
+        'valid_tickers.csv', index_col=0, squeeze=True, header=None))
+    return [i in valid_tickers for i in tickers]
 
 
 def save_tickers(n, filename='tickers.txt'):
+    """
+    Gets a csv of tickers from nasdaq.com and filters them based on whether they are valid or not. Then, outputs the first n to a file 'tickers.txt'.
+    """
     url = r'https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download'
     data = requests.get(url, allow_redirects=True)
     # read csv from a string
     df = pd.read_csv(StringIO(data.text))
     # filter out tickers which are valid and send to a file
-    print('\n'.join(df[is_valid(df['Symbol'])].loc[:n-1, 'Symbol']),
-          file=open(filename, 'w'))
+    df[filter_valid(df['Symbol'])].loc[:n-1, 'Symbol'].to_csv(filename,
+                                                              index=False,
+                                                              header=False)
 
 
 if __name__ == "__main__":
